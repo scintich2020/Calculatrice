cf5b00d3f0db971a8134e2d8d928395312e06774
Ulrich-bit <72482298+Ulrich-bit@users.noreply.github.com>
2020-10-30 11:19:33 +0100

    copie des nouveaux fichiers

---
+++ js/script.js
@@ -0,0 +1,8 @@
+console.log("Hello world");
+function value(valeur){
+    console.log(valeur);
+    var button=document.querySelectorAll('.button');
+    console.log(button);
+}
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
